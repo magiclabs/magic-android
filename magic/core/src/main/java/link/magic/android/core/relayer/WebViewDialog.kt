59b1ac54c535ddeabf419eb8f6b1399735222310
Arian Flores <arian.flores@magic.link>
2023-02-01 15:01:02 -0800

    Creates Custom WebViewDialog to present WebView

---
+++ magic/core/src/main/java/link/magic/android/core/relayer/WebViewDialog.kt
@@ -0,0 +1,25 @@
+package link.magic.android.core.relayer
+
+import android.app.Dialog
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.webkit.WebView
+import android.widget.RelativeLayout
+
+class WebViewDialog(context: Context, private val webView: WebView) : Dialog(context){
+    init {
+        setContentView(createContentView())
+        setCancelable(true)
+        window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        window?.setBackgroundDrawableResource(android.R.color.transparent)
+        window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+    }
+
+    private fun createContentView(): View {
+        val layout = RelativeLayout(context)
+        layout.addView(webView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        return layout
+    }
+}
