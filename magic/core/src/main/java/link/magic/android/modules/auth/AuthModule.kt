2bce20a4aec1bf203c48100ab2264b95a7f7762b
Arian Flores <arian.flores@magic.link>
2023-02-27 14:19:32 -0800

    Adds Warning Logs to Modules

--- magic/core/src/main/java/link/magic/android/modules/auth/AuthModule.kt
+++ magic/core/src/main/java/link/magic/android/modules/auth/AuthModule.kt
@@ -1,29 +1,40 @@
 package link.magic.android.modules.auth
 
 import android.content.Context
+import android.util.Log
 import link.magic.android.core.provider.RpcProvider
 import link.magic.android.modules.BaseModule
 import link.magic.android.modules.auth.requestConfiguration.LoginWithEmailOTPConfiguration
 import link.magic.android.modules.auth.requestConfiguration.LoginWithMagicLinkConfiguration
 import link.magic.android.modules.auth.requestConfiguration.LoginWithSMSConfiguration
 import link.magic.android.modules.auth.response.DIDToken
+import link.magic.android.modules.user.UserModule
 import org.web3j.protocol.core.Request
 import java.util.concurrent.CompletableFuture
-
+/**
+ * AuthModule and it's methods only work with Magic Auth API Keys
+ */
 class AuthModule(rpcProvider: RpcProvider) : BaseModule(rpcProvider) {
+
+    private val TAG: String? = "Magic SDK ${AuthModule::class.java.getName()}"
+    private val LOG_WARN_MSG: String = "This method only works with Magic Auth API Keys"
+
     fun loginWithMagicLink(context: Context, configuration: LoginWithMagicLinkConfiguration): CompletableFuture<DIDToken> {
+        Log.w(TAG, LOG_WARN_MSG)
         val request = Request(Method.MAGIC_AUTH_LOGIN_WITH_MAGIC_LINK.toString(), listOf(configuration), provider, DIDToken::class.java)
         provider.context = context
         return provider.sendAsync(request, DIDToken::class.java)
     }
 
     fun loginWithSMS(context: Context, configuration: LoginWithSMSConfiguration): CompletableFuture<DIDToken> {
+        Log.w(TAG, LOG_WARN_MSG)
         val request = Request(Method.MAGIC_AUTH_LOGIN_WITH_SMS.toString(), listOf(configuration), provider, DIDToken::class.java)
         provider.context = context
         return provider.sendAsync(request, DIDToken::class.java)
     }
 
     fun loginWithEmailOTP(context: Context, configuration: LoginWithEmailOTPConfiguration): CompletableFuture<DIDToken> {
+        Log.w(TAG, LOG_WARN_MSG)
         val request = Request(Method.MAGIC_AUTH_LOGIN_WITH_EMAIL_OTP.toString(), listOf(configuration), provider, DIDToken::class.java)
         provider.context = context
         return provider.sendAsync(request, DIDToken::class.java)
--- magic/core/src/main/java/link/magic/android/modules/wallet/WalletModule.kt
+++ magic/core/src/main/java/link/magic/android/modules/wallet/WalletModule.kt
@@ -1,35 +1,48 @@
 package link.magic.android.modules.wallet
 
 import android.content.Context
+import android.util.Log
 import link.magic.android.core.provider.RpcProvider
 import link.magic.android.modules.BaseModule
+import link.magic.android.modules.user.UserModule
 import link.magic.android.modules.wallet.requestConfiguration.RequestUserInfoWithUIConfiguration
 import link.magic.android.modules.wallet.response.*
 import org.web3j.protocol.core.Request
 import java.util.concurrent.CompletableFuture
-
+/**
+ * WalletModule and it's methods only work with Magic Connect API Keys
+ */
 class WalletModule(rpcProvider: RpcProvider) : BaseModule(rpcProvider) {
+
+    private val TAG: String? = "Magic SDK ${WalletModule::class.java.getName()}"
+    private val LOG_WARN_MSG: String = "This method only works with Magic Connect API Keys"
+
     fun connectWithUI(context: Context): CompletableFuture<ConnectWithUIResponse> {
+        Log.w(TAG, LOG_WARN_MSG)
         provider.context = context
         val request = Request(Method.MC_LOGIN.toString(), emptyList<String>(), provider, ConnectWithUIResponse::class.java)
         return provider.sendAsync(request, ConnectWithUIResponse::class.java)
     }
     fun getInfo(context: Context): CompletableFuture<WalletInfoResponse> {
+        Log.w(TAG, LOG_WARN_MSG)
         provider.context = context
         val request = Request(Method.MC_GET_WALLET_INFO.toString(), emptyList<String>(), provider, WalletInfoResponse::class.java)
         return provider.sendAsync(request, WalletInfoResponse::class.java)
     }
     fun showUI(context: Context): CompletableFuture<ShowWalletResponse> {
+        Log.w(TAG, LOG_WARN_MSG)
         provider.context = context
         val request = Request(Method.MC_WALLET.toString(), emptyList<String>(), provider, ShowWalletResponse::class.java)
         return provider.sendAsync(request, ShowWalletResponse::class.java)
     }
     fun requestUserInfoWithUI(context: Context, configuration: RequestUserInfoWithUIConfiguration?): CompletableFuture<RequestUserInfoWithUIResponse> {
+        Log.w(TAG, LOG_WARN_MSG)
         provider.context = context
         val request = Request(Method.MC_REQUEST_USER_INFO.toString(), ( if (configuration != null) listOf(configuration) else emptyList<String>()), provider, RequestUserInfoWithUIResponse::class.java)
         return provider.sendAsync(request, RequestUserInfoWithUIResponse::class.java)
     }
     fun disconnect(context: Context): CompletableFuture<DisconnectResponse> {
+        Log.w(TAG, LOG_WARN_MSG)
         provider.context = context
         val request = Request(Method.MC_DISCONNECT.toString(), emptyList<String>(), provider, DisconnectResponse::class.java)
         return provider.sendAsync(request, DisconnectResponse::class.java)
