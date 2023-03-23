7f25b389258a9ede1ec38e4ecf4fc9c978c6ae4c
Arian Flores <arian.flores@magic.link>
2023-03-22 18:13:27 -0700

    Adds helpful initialization Logic to Demo App

--- example/src/main/java/link/magic/demo/MainActivity.kt
+++ example/src/main/java/link/magic/demo/MainActivity.kt
@@ -1,13 +1,32 @@
 package link.magic.demo
 
 import android.os.Bundle
+import android.util.Log
+import link.magic.DemoApp
+import link.magic.android.Magic
+import link.magic.android.modules.user.response.IsLoggedInResponse
 
 
 open class MainActivity : UtilActivity() {
+    lateinit var magic: Any
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         startMALoginActivity()
+
+// Utlize this when using MA API Keys
+//        magic = (applicationContext as DemoApp).magic
+//        val completable = (magic as Magic).user.isLoggedIn(this)
+//        completable.whenComplete { response: IsLoggedInResponse?, error: Throwable? ->
+//            if (error != null) {
+//                Log.d("error", error.localizedMessage)
+//            }
+//            if (response != null && response.result) {
+//                startTabActivity()
+//            } else {
+//                startMALoginActivity()
+//            }
+//        }
     }
 }
