f2e6019f33d07c47c2018b2c280652e8342a87fc
ethella <jerry@magic.link>
2023-07-19 13:25:28 -0700

    remove rt

--- magic/core/src/main/java/link/magic/android/core/relayer/message/MagicRequestData.kt
+++ magic/core/src/main/java/link/magic/android/core/relayer/message/MagicRequestData.kt
@@ -3,7 +3,7 @@ import androidx.annotation.Keep
 import link.magic.android.core.provider.RequestForSerialization
 
 @Keep
-internal class MagicRequestData(val payload: RequestForSerialization<*>, outboundMessageType: OutboundMessageType, encodedParams: String, val rt: String? = null) {
+internal class MagicRequestData(val payload: RequestForSerialization<*>, outboundMessageType: OutboundMessageType, encodedParams: String) {
     val msgType: String = "$outboundMessageType-$encodedParams"
     val jwt: String = DPop.createJwt()
 }
