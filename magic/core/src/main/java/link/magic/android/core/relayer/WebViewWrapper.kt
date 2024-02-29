26fb5537405c2f569fae1b0419b4ac13a00ae1bd
Arian Flores <arian.flores@magic.link>
2024-02-28 16:07:36 -0800

    Update WebViewWrapper

--- magic/core/src/main/java/link/magic/android/core/relayer/WebViewWrapper.kt
+++ magic/core/src/main/java/link/magic/android/core/relayer/WebViewWrapper.kt
@@ -22,7 +22,7 @@ import androidx.webkit.WebViewFeature.WEB_MESSAGE_PORT_POST_MESSAGE
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import link.magic.android.Magic
-import link.magic.android.core.relayer.message.AnnouncementResponse
+import link.magic.android.core.relayer.message.AnnoucementResult
 import link.magic.android.core.relayer.message.InboundMessageType
 import link.magic.android.core.relayer.message.ResponseData
 import link.magic.android.core.relayer.urlBuilder.URLBuilder
@@ -153,7 +153,8 @@ class WebViewWrapper internal constructor(context: Context, private val urlBuild
             }
             InboundMessageType.MAGIC_SEND_PRODUCT_ANNOUNCEMENT.toString() in response.msgType -> {
                 val TAG: String = "Magic SDK ${WebViewWrapper::class.java.name}"
-                val announcement = Gson().fromJson<ResponseData<AnnouncementResponse>>(message, AnnouncementResponse::class.java)
+                val type = object : TypeToken<ResponseData<Response<AnnoucementResult>>>() {}.type
+                val announcement = Gson().fromJson<ResponseData<Response<AnnoucementResult>>>(message, type)
                 Log.w(TAG, announcement.response.result.product_announcement)
             }
         }
