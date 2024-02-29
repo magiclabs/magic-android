e8a0a8bdac73e52ae0f09dda3bf2d6cc539f7ea1
Arian Flores <arian.flores@magic.link>
2024-02-28 16:07:16 -0800

    Use AnnoucementResult

--- magic/core/src/main/java/link/magic/android/core/relayer/message/Announcement.kt
+++ magic/core/src/main/java/link/magic/android/core/relayer/message/Announcement.kt
@@ -1,11 +1,5 @@
 package link.magic.android.core.relayer.message
 
-data class AnnouncementResponse(
-    val jsonrpc: String,
-    val id: Int?, // Assuming id can be null based on your example
-    val result: Result
-)
-
-data class Result(
+data class AnnoucementResult(
     val product_announcement: String
 )
