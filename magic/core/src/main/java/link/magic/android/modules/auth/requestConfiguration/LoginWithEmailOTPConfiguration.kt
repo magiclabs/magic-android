46e7d405be10fc9de475a35c5bb33b7e4df7a598
Jerry Liu <33166884+Ethella@users.noreply.github.com>
2024-03-26 16:45:32 -0400

    Pdeexp 691 email template on android sdk (#42)
    * Add overrides.variation to EmailOTP
    
    * Add overrides.variation to EmailOTP
    
    * bump version

--- magic/core/src/main/java/link/magic/android/modules/auth/requestConfiguration/LoginWithEmailOTPConfiguration.kt
+++ magic/core/src/main/java/link/magic/android/modules/auth/requestConfiguration/LoginWithEmailOTPConfiguration.kt
@@ -3,4 +3,7 @@ package link.magic.android.modules.auth.requestConfiguration
 import androidx.annotation.Keep
 
 @Keep
-class LoginWithEmailOTPConfiguration (var email: String)
+class LoginWithEmailOTPConfiguration (var email: String, var overrides: OverridesConfiguration? = null)
+
+@Keep
+class OverridesConfiguration(var variation: String)
