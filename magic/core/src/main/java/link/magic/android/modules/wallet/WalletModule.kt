e44026ccff985298cbeca2b0998a425022b6da05
romin-halltari <150072475+romin-halltari@users.noreply.github.com>
2024-07-16 17:15:55 -0700

    fix: add config param to showUI (#52)

--- magic/core/src/main/java/link/magic/android/modules/wallet/WalletModule.kt
+++ magic/core/src/main/java/link/magic/android/modules/wallet/WalletModule.kt
@@ -7,6 +7,18 @@ import link.magic.android.modules.wallet.requestConfiguration.RequestUserInfoWit
 import link.magic.android.modules.wallet.response.*
 import org.web3j.protocol.core.Request
 import java.util.concurrent.CompletableFuture
+
+interface ShowUiConfig {
+    val onramperParams: Map<String, String>?
+}
+
+// Default implementation of ShowUiConfig
+object DefaultShowUiConfig : ShowUiConfig {
+    override val onramperParams: Map<String, String> = emptyMap()
+}
+
+class CustomShowUiConfig(override val onramperParams: Map<String, String>) : ShowUiConfig
+
 /**
  * WalletModule and it's methods only work with Magic Connect API Keys
  */
@@ -21,9 +33,9 @@ class WalletModule(rpcProvider: RpcProvider) : BaseModule(rpcProvider) {
         val request = Request(Method.MC_GET_WALLET_INFO.toString(), emptyList<String>(), provider, WalletInfoResponse::class.java)
         return provider.sendAsync(request, WalletInfoResponse::class.java)
     }
-    fun showUI(context: Context, onramperParams: Map<String, String> = emptyMap()): CompletableFuture<ShowWalletResponse> {
+    fun showUI(context: Context, config: ShowUiConfig = DefaultShowUiConfig): CompletableFuture<ShowWalletResponse> {
         provider.context = context
-        val request = Request(Method.MC_WALLET.toString(), listOf( mapOf("onramperParams" to onramperParams) ), provider, ShowWalletResponse::class.java)
+        val request = Request(Method.MC_WALLET.toString(), listOf(config), provider, ShowWalletResponse::class.java)
         return provider.sendAsync(request, ShowWalletResponse::class.java)
     }
     fun requestUserInfoWithUI(context: Context, configuration: RequestUserInfoWithUIConfiguration?): CompletableFuture<RequestUserInfoWithUIResponse> {
